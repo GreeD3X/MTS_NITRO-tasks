1280f662fba9df30f057f3c53201b231d9e60f79
GreeD3X <griganic@gmail.com>
2022-09-15 14:52:03 +0300

    second task

--- SecondTask/Program.cs
+++ SecondTask/Program.cs
@@ -5,37 +5,32 @@ using System.Globalization;
 
 
 class Program
-
 {
 
     static readonly IFormatProvider _ifp = CultureInfo.InvariantCulture;
 
-
-
     class Number
-
     {
-
         readonly int _number;
 
-
-
         public Number(int number)
-
         {
-
             _number = number;
-
         }
 
-
-
-        public override string ToString()
-
+        public Number(string s)
         {
+            _number = Int32.Parse(s);
+        }
 
+        public static string operator +(Number a, Number b) => new Number(a._number + b._number);
+
+        public static string operator +(Number a, string b) => a + new Number(b);
+
+        public static implicit operator string(Number a) => a.ToString();
+        public override string ToString()
+        {
             return _number.ToString(_ifp);
-
         }
 
     }
@@ -43,19 +38,13 @@ class Program
 
 
     static void Main(string[] args)
-
     {
-
         int someValue1 = 10;
-
         int someValue2 = 5;
-
-
 
         string result = new Number(someValue1) + someValue2.ToString(_ifp);
 
         Console.WriteLine(result);
-
         Console.ReadKey();
 
     }
