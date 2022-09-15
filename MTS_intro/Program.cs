4180967e26ec9ac41e4df5ad527691becd935fd1
GreeD3X <griganic@gmail.com>
2022-09-15 13:53:36 +0300

    FirstTask

---
+++ MTS_intro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        try
+
+        {
+
+            FailProcess();
+
+        }
+
+        catch { }
+
+
+
+        Console.WriteLine("Failed to fail process!");
+
+        Console.ReadKey();
+
+    }
+
+
+
+    static void FailProcess()
+    {       //... write your code here 	} 
+
+
+        //Environment.Exit(0);
+
+
+        /*System.Diagnostics.Process proc = System.Diagnostics.Process.GetCurrentProcess();
+        proc.Kill();*/
+
+
+        //System.Environment.FailFast("Sweet release of death"); //-- exception party
+
+
+
+    }
+}
