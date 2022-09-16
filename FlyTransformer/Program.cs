030042caf69a1fe16c28921f8087eb22762136ec
GreeD3X <griganic@gmail.com>
2022-09-16 16:17:04 +0300

    flyTransformer: early

---
+++ FlyTransformer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TransformToElephant();
+        Console.WriteLine("Муха");
+        //... custom application code 
+    }
+
+    static void TransformToElephant()
+    {       //... write your code here 	} 
+        Console.WriteLine("elephanto\r");
+        Console.Write("\x1B[1D\x1B[1D\x1B[1D\x1B[1P\x1B[1P\x1B[1P\x1B[1P");
+        //Console.SetOut(TextWriter.Null); //bad one(
+    }
+}
---
+++ Fourth task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirdTask
+{
+    static class Program
+    {
+        /// <summary> 
+        /// Возвращает отсортированный по возрастанию поток чисел 
+        /// </summary> 
+        /// <param name="inputStream">Поток чисел от 0 до maxValue. Длина потока не превышает миллиарда чисел.</param> 
+        /// <param name="sortFactor">Фактор упорядоченности потока. Неотрицательное число. Если в потоке встретилось число x, то в нём больше не встретятся числа меньше, чем (x - sortFactor).</param> 
+        /// <param name="maxValue">Максимально возможное значение чисел в потоке. Неотрицательное число, не превышающее 2000.</param> 
+        /// <returns>Отсортированный по возрастанию поток чисел.</returns> 
+        IEnumerable<int> Sort(IEnumerable<int> inputStream, int sortFactor, int maxValue) 
+        {
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
